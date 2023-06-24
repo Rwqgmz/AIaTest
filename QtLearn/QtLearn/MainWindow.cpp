7b4ac293de2a48739486061a3f3a58221581612f
Rwqgmz <42513854+Rwqgmz@users.noreply.github.com>
2023-06-25 01:12:37 +0800

    增加按钮实现

--- QtLearn/QtLearn/MainWindow.cpp
+++ QtLearn/QtLearn/MainWindow.cpp
@@ -1,9 +1,16 @@
 #include "MainWindow.h"
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    m_widget = new QWidget(ui.centralWidget);
+    m_widget->setWindowIconText(QStringLiteral("½çÃæ"));
+
+    QPushButton* button = new QPushButton(m_widget);
+    button->setText(QStringLiteral("°´Å¥"));
 }
 
 MainWindow::~MainWindow()
